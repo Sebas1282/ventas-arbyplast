3d8c6c7759c03f1bd9008e3b1fee65bdce2c930e
Sebas1282 <juanse2012071282@gmail.com>
2017-09-14 15:13:29 -0500

    creacion de modulos y componentes con su correspondiente registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BodegaModule}from './bodega/bodega.module'
+import {CategoriaProductoModule} from './categoria-producto/categoria-producto.module'
+import {ClienteModule} from './cliente/cliente.module'
+import {DistribuidorModule}from './distribuidor/distribuidor.module'
+import {PreventaModule}from './preventa/preventa.module'
+import {ProductoDistribuidorModule} from './producto-distribuidor/producto-distribuidor.module'
+import{UsuarioModule} from './usuario/usuario.module'
+import{VentaClienteModule} from './venta-cliente/venta-cliente.module';
+import { MenuComponent } from './menu/menu.component';
+import { OpcionSeleccionadaComponent } from './opcion-seleccionada/opcion-seleccionada.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    OpcionSeleccionadaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BodegaModule,
+    CategoriaProductoModule,
+    ClienteModule,
+    DistribuidorModule,
+    PreventaModule,
+    ProductoDistribuidorModule,
+    UsuarioModule,
+    VentaClienteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
